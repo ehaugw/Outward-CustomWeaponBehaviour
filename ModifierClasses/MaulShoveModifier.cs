9f90a52aca8d5a368b2cc1f8dd5139c1eb4fa705
Eivind <ehaugw@gmail.com>
2024-07-16 14:50:46 +0200

    make it so maul shove with all weapons off same material deal about the same impact

--- ModifierClasses/MaulShoveModifier.cs
+++ ModifierClasses/MaulShoveModifier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TinyHelper;
 
 namespace CustomWeaponBehaviour
 {
@@ -9,7 +10,7 @@ namespace CustomWeaponBehaviour
     {
         public void ApplyDamageModifier(Weapon weapon, DamageList original, ref DamageList result)
         {
-            result += original * -0.25f;
+            result += original * -0.5f;
         }
 
         public void ApplyImpactModifier(Weapon weapon, float original, ref float result)
@@ -17,14 +18,21 @@ namespace CustomWeaponBehaviour
             switch (weapon.Type)
             {
                 case Weapon.WeaponType.Sword_1H:
+                    result += original * (WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_2H] / WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Sword_1H] * 2f - 1);
+                    break;
                 case Weapon.WeaponType.Sword_2H:
+                    result += original * (WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_2H] / WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Sword_2H] * 1.18f - 1);
+                    break;
                 case Weapon.WeaponType.Axe_1H:
+                    result += original * (WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_2H] / WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Axe_1H] * 1.9f - 1);
+                    break;
                 case Weapon.WeaponType.Axe_2H:
-                    result += original * 2f;
+                    result += original * (WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_2H] / WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Axe_2H] * 1.29f - 1);
                     break;
                 case Weapon.WeaponType.Mace_1H:
+                    result += original * (WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_2H] / WeaponManager.HeavyImpactModifiers[Weapon.WeaponType.Mace_1H] * 1.24f - 1);
+                    break;
                 case Weapon.WeaponType.Mace_2H:
-                    result += original * 0.5f;
                     break;
                 default:
                     break;
